a0a65b714a0c8824ecbb5cbc0559964347d78637
nimapm <nima.gt10@gmail.com>
2022-05-14 22:07:36 +0430

    Update

--- src/App.js
+++ src/App.js
@@ -7,31 +7,31 @@ import ProjectsView from "./components/projects/ProjectsView";
 import BlogGrid from "./components/blog/BlogGrid";
 import BlogView from "./components/blog/BlogView";
 import Performance from "./components/Performance";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div>
                 <Routes>
-                    <Route exact path="npm-screen/" element={<Home />} />
-                    <Route path="npm-screen/introduction" element={<IntroductionKit />} />
-                    <Route path="npm-screen/about" element={<About />} />
-                    <Route path="npm-screen/performance" element={<Performance />} />
+                    <Route exact path="/npm-screen/" element={<Home />} />
+                    <Route path="/npm-screen/introduction" element={<IntroductionKit />} />
+                    <Route path="/npm-screen/about" element={<About />} />
+                    <Route path="/npm-screen/performance" element={<Performance />} />
 
-                    <Route path="npm-screen/blog">
+                    <Route path="/npm-screen/blog">
                         <Route index element={<BlogGrid />} />
-                        <Route path="npm-screen/post/:id" element={<BlogView />} />
+                        <Route path="/npm-screen/post/:id" element={<BlogView />} />
                     </Route>
 
-                    <Route path="npm-screen/projects">
+                    <Route path="/npm-screen/projects">
                         <Route index element={<ProjectsGrid />} />
-                        <Route path="npm-screen/projects/:id" element={<ProjectsView />} />
+                        <Route path="/npm-screen/projects/:id" element={<ProjectsView />} />
                     </Route>
                 </Routes>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
